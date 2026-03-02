88d0f9cd37e773f4f7d2172f93629998dfb3c8e6
Romke Gionet-Stel <romkestel@icloud.com>
2026-03-02 15:17:54 -0400

    Hopefully finished trainer.py

--- trainer.py
+++ trainer.py
@@ -8,14 +8,17 @@ from taxi_dataset import TaxiDataset
 device = torch.device(
     "cuda"
     if torch.cuda.is_available()
-    else "mps" if torch.backends.mps.is_available() else "cpu"
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
 )
 
 train_ds = TaxiDataset("./Dataset/train_compressed.parquet")
 train_loader = DataLoader(train_ds, batch_size=4096, shuffle=True, num_workers=4)
 
 
-cat_dims = [270, 270, 5, 8, 8, 8]
+cat_dims = (train_ds.cat_features.max(dim=0)[0] + 1).tolist()
+print(f"Dynamic Embedding Size: {cat_dims}")
 model = WideAndDeepModel(cat_dims=cat_dims, num_cont=9).to(device)
 
 
@@ -27,7 +30,7 @@ for epoch in range(7):
     epoch_loss = 0
 
     for cat, cont, target in train_loader:
-        cat, cont, target = cat.to(device), cont.to(device), target.to(device)
+        cat, cont, target = cat.to(device), cont.to(device), target.squeeze().to(device)
 
         preds = model(cat, cont)
         loss = criterion(preds, target)
